b172f438aede388d7277a3ef00b317c710cdb798
glyphaj <glyphaj10@outlook.com>
2024-11-11 16:28:37 +0000

    Add files via upload

---
+++ js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'@glyphaj','AJ','"professinal sigma"','cool person',':))','Join the discord LOLLLLLLLL','https://discord.gg/C6KDYq9Peb :D'],
+  typeSpeed : 50,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
